e3c04e9104211cf8bcd4420c3257f0fd4a7e9765
zuozuosama <50353879+zuozuosama@users.noreply.github.com>
2020-09-11 14:31:22 +0800

    Update Case1.java

--- src/algorithm/tonghuashun/Case1.java
+++ src/algorithm/tonghuashun/Case1.java
@@ -2,7 +2,7 @@ package algorithm.tonghuashun;
 
 public class Case1 {
     public static void printNotContainFive(){
-        for (int i = 1; i <= 999; i++) {
+        for (int i = 1; i <= 900; i++) {
             String s = String.valueOf(i);
             if (!s.contains("5")){
                 System.out.println(s);
