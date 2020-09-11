a8846ae9e1d421fc7b356dc59d804e1ea913926b
lizq9 <lizq9@seentao.com>
2020-09-11 14:37:15 +0800

    同花顺

--- src/algorithm/tonghuashun/Case1.java
+++ src/algorithm/tonghuashun/Case1.java
@@ -1,8 +1,9 @@
 package algorithm.tonghuashun;
 
 public class Case1 {
+    private String d;
     public static void printNotContainFive(){
-        for (int i = 1; i <= 900; i++) {
+        for (int i = 1; i <= 909; i++) {
             String s = String.valueOf(i);
             if (!s.contains("5")){
                 System.out.println(s);
