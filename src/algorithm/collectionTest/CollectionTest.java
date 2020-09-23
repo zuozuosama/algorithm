932a0abf04b8cd3a55b67188934b3527c6f8036b
lizq9 <lizq9@seentao.com>
2020-09-23 13:14:23 +0800

    集合类

---
+++ src/algorithm/collectionTest/CollectionTest.java
@@ -0,0 +1,11 @@
+package algorithm.collectionTest;
+
+import java.util.*;
+
+public class CollectionTest {
+    public static void main(String[] args) {
+        List<Integer> integers = new ArrayList<>();  // extends AbstractList implements List extends Collection
+        Set<Integer> integers1 = new HashSet<>();  //extends AbstractSet implements Set extends Collection
+        Map<Integer, Integer> integerIntegerHashMap = new HashMap<>();  //extends AbstractMap implements Map
+    }
+}
