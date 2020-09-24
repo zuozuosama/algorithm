c16650558a98c906e4ea7d46e3db7ba1d2514b67
lizq9 <lizq9@seentao.com>
2020-09-24 10:01:20 +0800

    问境科技

--- src/algorithm/collectionTest/CollectionTest.java
+++ src/algorithm/collectionTest/CollectionTest.java
@@ -6,6 +6,7 @@ public class CollectionTest {
     public static void main(String[] args) {
         List<Integer> integers = new ArrayList<>();  // extends AbstractList implements List extends Collection
         Set<Integer> integers1 = new HashSet<>();  //extends AbstractSet implements Set extends Collection
-        Map<Integer, Integer> integerIntegerHashMap = new HashMap<>();  //extends AbstractMap implements Map
+        HashMap<Integer, Integer> integerIntegerHashMap = new HashMap<>();  //extends AbstractMap implements Map
+        integerIntegerHashMap.put(2,2);
     }
 }
