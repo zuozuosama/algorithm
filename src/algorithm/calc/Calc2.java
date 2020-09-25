d037bcfbf7ba96a18f80c9796be236cf0c216c04
lizq9 <lizq9@seentao.com>
2020-09-25 16:47:44 +0800

    计算器

--- src/algorithm/calc/Calc2.java
+++ src/algorithm/calc/Calc2.java
@@ -3,9 +3,11 @@ package algorithm.calc;
 import java.util.Stack;
 
 /**
- * 处理乘除法
+ * 处理乘除法和空格
  */
 public class Calc2 {
+    private static int i = 0;
+
     public static int calculate(String s) {
         Stack<Integer> stk = new Stack<>();
         // 记录算式中的数字
@@ -13,14 +15,21 @@ public class Calc2 {
         // 记录 num 前的符号，初始化为 +
         char sign = '+';
         int pre;
-        for (int i = 0; i < s.length(); i++) {
+        for (; i < s.length(); i++) {
             char c = s.charAt(i);
+            if (c == ' ') continue;
             // 如果是数字，连续读取到 num
-            if (c >= 48 && c <= 57)
+            if (Character.isDigit(c))
                 num = 10 * num + (c - '0');
+
+            if (c == '(') {
+                i++;
+                num = calculate(s);
+            }
+
             // 如果不是数字，就是遇到了下一个符号，
             // 之前的数字和符号就要存进栈中
-            if ((c < 48 || c > 57) || i == s.length() - 1) {
+            if (!Character.isDigit(c) || i == s.length() - 1) {
                 switch (sign) {
                     case '+':
                         stk.push(num);
@@ -44,18 +53,20 @@ public class Calc2 {
                 sign = c;
                 num = 0;
             }
+
+            if (c == ')')
+                break;
         }
-        // 将栈中所有结果求和就是答案
         int res = 0;
+        // 将栈中所有结果求和就是答案
         while (!stk.empty()) {
-            res += stk.peek();
-            stk.pop();
+            res += stk.pop();
         }
         return res;
     }
 
     public static void main(String[] args) {
-        int calculate = calculate("1-12+3");
+        int calculate = calculate("1 -(12-2) +3");
         System.out.println(calculate);
     }
 }
