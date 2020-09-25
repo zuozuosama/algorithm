11262944a734bea434d8c7b09f81c30e94eb0e7a
lizq9 <lizq9@seentao.com>
2020-09-25 14:48:19 +0800

    位运算

---
+++ src/algorithm/bitOperation/HammingWeight.java
@@ -0,0 +1,15 @@
+package algorithm.bitOperation;
+
+/**
+ * 编写一个函数，输入是一个无符号整数，返回其二进制表达式中数字位数为 ‘1’ 的个数（也被称为汉明重量）。
+ */
+public class HammingWeight {
+    public int hammingWeight(int n) {
+        int res = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            res++;
+        }
+        return res;
+    }
+}
