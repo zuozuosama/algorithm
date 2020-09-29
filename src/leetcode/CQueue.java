cfa426d2117692b0cc66ec1f50c0b42eca05533c
lizq9 <lizq9@seentao.com>
2020-09-29 12:25:22 +0800

    leetcode

---
+++ src/leetcode/CQueue.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import java.util.LinkedList;
+
+/**
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+ * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ */
+public class CQueue {
+
+    private LinkedList<Integer> stack1;
+    private LinkedList<Integer> stack2;
+
+    public CQueue() {
+        stack1 = new LinkedList<>();
+        stack2 = new LinkedList<>();
+    }
+
+    public void appendTail(int value) {
+        stack1.add(value);
+    }
+
+    public int deleteHead() {
+        if (stack2.isEmpty()) {
+            if (stack1.isEmpty()) return -1;
+            while (!stack1.isEmpty()) {
+                stack2.add(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public static void main(String[] args) {
+        CQueue cQueue = new CQueue();
+        cQueue.appendTail(2);
+        cQueue.appendTail(6);
+        cQueue.appendTail(3);
+        cQueue.appendTail(4);
+        System.out.println(cQueue.deleteHead());
+    }
+
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */
