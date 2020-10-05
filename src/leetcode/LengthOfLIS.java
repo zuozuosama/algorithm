e01ecedc88bb3055974b01b34b051d2214e41a79
lizq9 <lizq9@seentao.com>
2020-10-05 10:59:02 +0800

    Leetcode

---
+++ src/leetcode/LengthOfLIS.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 300. 最长上升子序列
+ *
+ *  输入: [10,9,2,5,3,7,101,18]
+ *  输出: 4
+ *  解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+ */
+public class LengthOfLIS {
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        if (len < 2) {
+            return len;
+        }
+        int[] dp = new int[len];
+        // 自己一定是一个子序列
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < len; i++) {
+            // 看以前的，比它小的，说明可以接在后面形成一个更长的子序列
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+        }
+
+        int res = dp[0];
+        for (int i = 0; i < len; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
