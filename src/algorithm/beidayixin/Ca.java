21066b329f1947b88c789cd83f1116cfe316ee76
lizq9 <lizq9@seentao.com>
2020-11-07 14:09:13 +0800

    牛客网

---
+++ src/algorithm/beidayixin/Ca.java
@@ -0,0 +1,18 @@
+package algorithm.beidayixin;
+
+public class Ca {
+
+    protected class b{
+
+    }
+
+    public void ca(){
+
+    }
+    public static void main(String[] args) {
+        int i,j;
+        for(i=0,j=0;i+j<20;++i,j+=i--){
+            System.out.println(i+j);
+        }
+    }
+}
