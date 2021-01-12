cb617b008b72b08fceee4fca0da211e2b2903cb4
zuozuosama <atsl134zk>
2021-01-12 18:25:07 +0800

    lamda

--- src/algorithm/minshengyinhang/TwoDimensionalArraySort.java
+++ src/algorithm/minshengyinhang/TwoDimensionalArraySort.java
@@ -1,7 +1,6 @@
 package algorithm.minshengyinhang;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 /**
  * 对二维数组排序，民生银行总行笔试题
@@ -9,12 +8,9 @@ import java.util.Comparator;
 public class TwoDimensionalArraySort {
     public static void main(String[] args) {
         int[][] a = new int[][]{{10, 1}, {9, 3}, {9, 2}};
-        Arrays.sort(a, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0] == o2[0]) return o1[1] - o2[1];
-                return o1[0] - o2[0];
-            }
+        Arrays.sort(a, (o1, o2) -> {
+            if (o1[0] == o2[0]) return o1[1] - o2[1];
+            return o1[0] - o2[0];
         });
         for (int[] b : a) {
             String s = Arrays.toString(b);
