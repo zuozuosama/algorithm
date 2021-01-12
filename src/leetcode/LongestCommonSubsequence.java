ab93baeb5707143a52705411596e02fd254aa523
zuozuosama <atsl134zk>
2021-01-12 15:21:46 +0800

    labu

--- src/leetcode/LongestCommonSubsequence.java
+++ src/leetcode/LongestCommonSubsequence.java
@@ -1,4 +1,18 @@
 package leetcode;
 
 public class LongestCommonSubsequence {
+    public int longestCommonSubsequence(String str1, String str2) {
+        int m = str1.length(), n = str2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
 }
--- src/leetcode/MaxProfit.java
+++ src/leetcode/MaxProfit.java
@@ -1,4 +1,14 @@
 package leetcode;
 
 public class MaxProfit {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) return 0;
+        int curMin = prices[0];
+        int res = 0;
+        for (int i = 1; i < prices.length; i++) {
+            curMin = Math.min(prices[i], curMin);
+            res = Math.max(prices[i] - curMin, res);
+        }
+        return res;
+    }
 }
