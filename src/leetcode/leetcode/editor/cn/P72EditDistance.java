f499324b8b2d54425321aa18e7c0c5046bec3399
lizq <1269066488@qq.com>
2022-01-30 11:20:15 +0800

    dp

--- src/leetcode/leetcode/editor/cn/P72EditDistance.java
+++ src/leetcode/leetcode/editor/cn/P72EditDistance.java
@@ -46,19 +46,37 @@ package leetcode.leetcode.editor.cn;
 // 
 // Related Topics 字符串 动态规划
 
-public class P72EditDistance{
+public class P72EditDistance {
     public static void main(String[] args) {
         Solution solution = new P72EditDistance().new Solution();
         // TO TEST
     }
-    
 
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int minDistance(String word1, String word2) {
 
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int minDistance(String word1, String word2) {
+            int n = word1.length();
+            int m = word2.length();
+            int[][] dp = new int[n + 1][m + 1];
+            for (int i = 1; i < n + 1; i++) {
+                dp[i][0] = i;
+            }
+            for (int j = 1; j < m + 1; j++) {
+                dp[0][j] = j;
+            }
+            for (int i = 1; i < n + 1; i++) {
+                for (int j = 1; j < m + 1; j++) {
+                    if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else {
+                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    }
+                }
+            }
+            return dp[n][m];
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
