79fa127f1d0068bf78a3e630c6cf84f399cf5903
lizq <1269066488@qq.com>
2022-01-31 20:46:38 +0800

    1

---
+++ src/leetcode/leetcode/editor/cn/P215KthLargestElementInAnArray.java
@@ -0,0 +1,47 @@
+package leetcode.leetcode.editor.cn;
+//数组中的第K个最大元素
+////给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。 
+//
+// 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: [3,2,1,5,6,4] 和 k = 2
+//输出: 5
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+//输出: 4 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= k <= nums.length <= 104 
+// -104 <= nums[i] <= 104 
+// 
+// Related Topics 数组 分治 快速选择 排序 堆（优先队列）
+
+public class P215KthLargestElementInAnArray{
+    public static void main(String[] args) {
+        Solution solution = new P215KthLargestElementInAnArray().new Solution();
+        // TO TEST
+    }
+    
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
